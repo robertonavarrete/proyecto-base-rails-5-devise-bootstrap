a0060936f75a45f3846082bd7cf2db4f317fc042
klavikula <robertonavarrete@outlook.com>
2020-03-11 16:25:16 -0300

    adding filters to index

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -8,6 +8,9 @@ ActiveAdmin.register User do
   # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
   permit_params :email, :password
 
+  filter :email, as: :select
+  filter :created_at, as: :date_range
+
   controller do
       def update
         if (params[:user][:password].blank? && params[:user][:password_confirmation].blank?)
